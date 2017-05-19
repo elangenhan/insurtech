86bdf2373da5d741658de8bf5adf695317283481
Norris Sam Osarenkhoe <nso@de.ibm.com>
2017-05-19 19:54:34 +0200

    lazy animation added

--- scripts/at-animate.js
+++ scripts/at-animate.js
@@ -1,4 +1,14 @@
+$(document).ready(() => {
+	init();
+});
+
+function init() {
+	$(".footer").hide();
+}
+
 function micClick() {
     $("#micBtn").addClass('clicked');
+    $(".footer").addClass('active');
+    $(".footer").show('fade', 5000);
     console.log("muh");
 }
