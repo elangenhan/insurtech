874c62eb66fe4ba9cee42cf20a5116cc1234f1cb
Norris Sam Osarenkhoe <nso@de.ibm.com>
2017-05-20 12:19:18 +0200

    new build and lil fix

--- dist/scripts/animate.js
+++ dist/scripts/animate.js
@@ -1,2 +1,2 @@
-"use strict";function init(){$(".rightButton").hide(),$(".footer").hide()}function handleClick(){speechClick(),$(".jumboButton").fadeOut(1e3),$(".rightButton").fadeIn(1e3),$(".rightButton > .fa-stack").addClass("pulse"),$(".footer").addClass("active"),$(".footer").fadeIn(1e3),console.log("muh")}$(document).ready(function(){init()});
+"use strict";function init(){$(".rightButton").hide(),$(".footer").hide()}function handleClick(){speechClick(),0==first&&($(".jumboButton").fadeOut(1e3),$(".rightButton").fadeIn(1e3),$(".rightButton > .fa-stack").addClass("pulse"),$(".footer").addClass("active"),$(".footer").fadeIn(1e3),console.log("Only one"),first=1),console.log("muh")}var first=0;$(document).ready(function(){init()});
 //# sourceMappingURL=animate.js.map
