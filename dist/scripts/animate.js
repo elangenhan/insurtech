9ace5d499211cc43a556d03467b3e735703f244e
Norris Sam Osarenkhoe <nso@de.ibm.com>
2017-05-20 11:14:38 +0200

    new build

---
+++ dist/scripts/animate.js
@@ -0,0 +1,2 @@
+"use strict";function init(){$(".rightButton").hide(),$(".footer").hide()}function handleClick(){speechClick(),$(".jumboButton").fadeOut(1e3),$(".rightButton").fadeIn(1e3),$(".rightButton > .fa-stack").addClass("pulse"),$(".footer").addClass("active"),$(".footer").fadeIn(1e3),console.log("muh")}$(document).ready(function(){init()});
+//# sourceMappingURL=animate.js.map
--- dist/scripts/speechRecognition.js
+++ dist/scripts/speechRecognition.js
@@ -1,17 +1,2 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
 
-
-function speech(callback){
-    recognition.addEventListener('result', e => {
-        let transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('');
-
-        if (e.results[0].isFinal) {
-            callback(transcript);
-        }
-    });
-    recognition.start();
-}
+//# sourceMappingURL=speechRecognition.js.map
