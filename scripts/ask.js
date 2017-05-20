c1aca28d7e757fbc7a1b8467318d752c374f7044
elangenhan <erik.langenhan@web.de>
2017-05-20 13:38:12 +0200

    ask

--- scripts/ask.js
+++ scripts/ask.js
@@ -1,4 +1,4 @@
-let langs = ["#asktom","#fragtom","#chiedermitom", "#spørgsmåltom", "#demandeztom", "#cпросиtom", "#onatom", "#preguntatom"];
+let langs = ["#asktom","#fragtom","#chiedermitom", "#spørgsmåltom", "#demandeztom", "#cпросиtom", "#onatom", "#preguntatom", "#vraagtom", "#kysymystom", "#pitanjetom", "#frågatom", "#zapytaćtoma", "#freegjetom"];
 
 setInterval(function(){
     var rand = langs[Math.floor(Math.random() * langs.length)];
